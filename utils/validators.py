c63379f3f3d7da8f1d6b77579bef01284acc3bd0
Sibgatullah <sibgatullah881@gmail.com>
2025-11-28 22:56:38 +0600

    utilities for validating methods

---
+++ utils/validators.py
@@ -0,0 +1,8 @@
+import re
+
+def is_valid_email(email:str) -> bool:
+    pattern = r'^[\w\.-]+@[\w\.-]+\. \w+$'
+    return re.match(pattern,email)is not None
+
+def is_strong_password(password: str) -> bool:
+    return len(password) >=6
