435a306dbe7bda3b1a9f60c80aa8c8e3ae0a67cc
Sibgatullah.py <157821820+sibgatullah-py@users.noreply.github.com>
2025-12-05 23:57:09 +0600

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -4,5 +4,5 @@ def is_valid_email(email: str) -> bool:
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return re.match(pattern, email) is not None
 
-def is_strong_password(password: str) -> bool:
+def is_strong_password(password: str) -> bool: # This will take the password as string and return a boolean value
     return len(password) >= 6
